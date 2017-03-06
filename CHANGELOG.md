78e5f16883e497dfa6e3b9dad71f7c0a4d937c5a
benatespina <benatespina@gmail.com>
2017-03-06 17:50:53 +0100

    Removed  rule

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ This changelog references the relevant changes done between versions.
 To get the diff for a specific change, go to https://github.com/kreta/PhpCsFixerConfig/commit/XXX where XXX is the change hash 
 To get the diff between two versions, go to https://github.com/kreta/PhpCsFixerConfig/compare/v1.0.0...v1.1.0
 
+* 1.0.2
+    * Removed `simplified_null_return` rule.
 * 1.0.1
     * Removed `binary_operator_spaces` `align_equals` rule.
 * 1.0.0
--- src/Kreta/PhpCsFixerConfig/KretaConfig.php
+++ src/Kreta/PhpCsFixerConfig/KretaConfig.php
@@ -145,7 +145,7 @@ EOF;
             'semicolon_after_instruction'                 => true,
             'short_scalar_cast'                           => true,
             'silenced_deprecation_error'                  => false,
-            'simplified_null_return'                      => true,
+            'simplified_null_return'                      => false,
             'single_blank_line_before_namespace'          => true,
             'single_quote'                                => true,
             'space_after_semicolon'                       => true,
